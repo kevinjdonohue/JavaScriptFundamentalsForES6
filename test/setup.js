402286e02b746be5ec55d91bbbd4b31c291380d8
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-01-19 11:38:50 -0800

    temporarily commenting out all of the configuration related to React, Enzyme, etc.

--- test/setup.js
+++ test/setup.js
@@ -1,25 +1,25 @@
-import React from 'react';
+// import React from 'react';
 import chai, { expect } from 'chai';
-import { JSDOM } from 'jsdom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import { JSDOM } from 'jsdom';
+// import Enzyme, { shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
 
 chai.should();
 
-Enzyme.configure({ adapter: new Adapter() });
+// Enzyme.configure({ adapter: new Adapter() });
 
-const doc = new JSDOM('<!DOCTYPE html><html><body></body></html>');
-const win = doc.window;
+// const doc = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+// const win = doc.window;
 
-global.document = doc;
-global.window = win;
+// global.document = doc;
+// global.window = win;
 
-Object.keys(window).forEach((key) => {
-  if (!(key in global)) {
-    global[key] = window[key];
-  }
-});
+// Object.keys(window).forEach((key) => {
+//   if (!(key in global)) {
+//     global[key] = window[key];
+//   }
+// });
 
-global.React = React;
+// global.React = React;
 global.expect = expect;
-global.shallow = shallow;
+// global.shallow = shallow;
