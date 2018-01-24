9ed2bf400a41712d28bcd9c0c4faf1c39ee49325
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-01-23 20:42:28 -0800

    importing babel-polyfill as a dependency in order to create a generator

--- src/Generators/Generators.test.js
+++ src/Generators/Generators.test.js
@@ -1,21 +1,23 @@
+import 'babel-polyfill';
+
 describe('Generators Tests', () => {
-  // it('should return a sum', () => {
-  //   // arrange
-  //   const numbers = function* numbersGenerator() {
-  //     yield 1;
-  //     yield 2;
-  //     yield 3;
-  //     yield 4;
-  //   };
-  //   let sum = 0;
-  //   // act
-  //   const iterator = numbers();
-  //   let next = iterator.next();
-  //   while (!next.done) {
-  //     sum += next.value;
-  //     next = iterator.next();
-  //   }
-  //   // assert
-  //   sum.should.equal(1);
-  // });
+  it('should return a sum', () => {
+    // arrange
+    const numbers = function* numbersGenerator() {
+      yield 1;
+      yield 2;
+      yield 3;
+      yield 4;
+    };
+    let sum = 0;
+    // act
+    const iterator = numbers();
+    let next = iterator.next();
+    while (!next.done) {
+      sum += next.value;
+      next = iterator.next();
+    }
+    // assert
+    sum.should.equal(1);
+  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/index.jsx',
-  ],
+  entry: ['webpack-dev-server/client?http://localhost:8080', 'webpack/hot/only-dev-server', './src/index.jsx'],
   module: {
     rules: [
       // {
