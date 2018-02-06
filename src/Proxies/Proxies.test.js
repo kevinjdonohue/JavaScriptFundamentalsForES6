f5d5143895d42230f089b24b347930873f63e8e6
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-02-06 09:45:47 -0800

    adding additional proxy tests

--- src/Proxies/Proxies.test.js
+++ src/Proxies/Proxies.test.js
@@ -4,8 +4,24 @@ const unicorn = {
   legs: 4,
   color: 'brown',
   horn: true,
+  hornAttack: target => `${target.name} was obliterated!`,
 };
 
+// function proxy
+unicorn.hornAttack = new Proxy(unicorn.hornAttack, {
+  apply: (target, context, args) => {
+    if (context !== unicorn) {
+      return 'nobody can use unicorn horn but the unicorn';
+    }
+
+    return target.apply(context, args);
+  },
+});
+
+const thief = { name: 'Rupert' };
+thief.attack = unicorn.hornAttack;
+
+// object proxy
 const proxyUnicorn = new Proxy(unicorn, {
   get: (target, property) => {
     if (property === 'color') {
@@ -45,5 +61,21 @@ describe('Proxies Tests', () => {
     proxyUnicorn.legs.should.equal(5);
     proxyUnicorn.color.should.equal('awesome white');
     proxyUnicorn.horn.should.equal(true); // disallowing the set to occur but returning true to avoid error
+  });
+
+  it('unicorn should (still) be able to use hornAttack', () => {
+    // act
+    const result = unicorn.hornAttack(thief);
+
+    // assert
+    result.should.equal('Rupert was obliterated!');
+  });
+
+  it('thief should not be able to use hornAttack because of the function proxy', () => {
+    // act
+    const result = thief.attack(thief);
+
+    // assert
+    result.should.equal('nobody can use unicorn horn but the unicorn');
   });
 });
